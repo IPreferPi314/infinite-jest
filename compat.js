4b3293a9e2f031d27398e9d9712375adc073fb58
IPreferPi314 <81392327+IPreferPi314@users.noreply.github.com>
2024-06-29 16:06:00 -0600

    Update compat.js

--- compat.js
+++ compat.js
@@ -1,6 +1,6 @@
 function displCompat() {
-	var n1 = document.getElementByID("n1").value.trim();
-	var n2 = document.getElementByID("n2").value.trim();
+	var n1 = document.getElementByID("n1").value;
+	var n2 = document.getElementByID("n2").value;
 	
 	//DEBUG:
 	//n1 = "Joshua Gillis";
